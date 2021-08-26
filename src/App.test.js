90868c6c0a74deda2d7fabe3a32b295e1bcccce1
Muhammad Rajput <muhammadrajput99@gmail.com>
2021-08-26 11:41:20 -0400

    fixed messages

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learns react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I am on the feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
